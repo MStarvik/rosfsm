288d21879fcb87713c5c46b41f034f57e0e3e02b
Mikkel Stårvik <mikkel.starvik@gmail.com>
2021-10-19 11:32:23 +0200

    Fix setup file

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python2
+from setuptools import setup
+
+setup(name='rosfsm',
+      version='0.1',
+      description='ROS finite state machine',
+      url='http://github.com/MStarvik/rosfsm',
+      author='Mikkel Stårvik',
+      author_email='mikkel.starvik@gmail.com',
+      license='MIT',
+      packages=['rosfsm'],
+      zip_safe=False)
