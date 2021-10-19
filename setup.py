668710d9fb6e426b5dfe0f0b6d63ca4dc87c42de
Mikkel Stårvik <mikkel.starvik@gmail.com>
2021-10-19 11:52:59 +0200

    Fix fsm dependency

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(name='rosfsm',
       author_email='mikkel.starvik@gmail.com',
       license='MIT',
       packages=['rosfsm'],
-      install_requires=['git+https://github.com/MStarvik/fsm.git@master#egg=fsm'],
+      install_requires=['fsm @ git+https://github.com/MStarvik/fsm.git@master#egg=fsm'],
       zip_safe=False)
